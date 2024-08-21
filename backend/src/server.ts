af185b306fc0d6ecf99c5cc3787b43079b250c9c
Marcelo Magario <marcelo.magario@mzgroup.com>
2024-08-21 18:08:21 -0300

    add CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from 'path';
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 import dotenv from 'dotenv';
 import uuidRoutes from './routes/uuidRoutes';
 
@@ -9,12 +9,30 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware para permitir CORS
-app.use(cors());
+// Lista de origens permitidas
+const allowedOrigins: string[] = ['https://uuid-generator-frontend.onrender.com/', 'http://another-example.com'];
+
+// Função de configuração do CORS
+const corsOptions: CorsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allowed?: boolean) => void) => {
+    // Permite solicitações de origens na lista de permitidas ou sem origem
+    if (origin === undefined || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'), false);
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 app.use(express.json());
 
 // Usar as rotas definidas em uuidRoutes
 app.use('/api', uuidRoutes);
+
+app.get('/api/uuids', (req, res) => {
+  res.json(['uuid1', 'uuid2']);
+});
 
 // Servir arquivos estáticos do frontend
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
