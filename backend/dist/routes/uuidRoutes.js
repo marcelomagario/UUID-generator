80d3867253d4d14948a1647f5437788b43ec1909
Marcelo Magario <marcelo.magario@mzgroup.com>
2024-08-26 07:20:53 -0300

    show more UUIDs

--- backend/dist/routes/uuidRoutes.js
+++ backend/dist/routes/uuidRoutes.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const uuid_1 = require("uuid");
 const router = express_1.default.Router();
 router.get('/uuids', (req, res) => {
-    const uuids = Array.from({ length: 4 }, () => (0, uuid_1.v4)());
+    const uuids = Array.from({ length: 8 }, () => (0, uuid_1.v4)());
     res.json(uuids);
 });
 exports.default = router;
